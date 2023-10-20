a111ccfa1c08dc654f56550055251f2c7fc8eade
Finn <Finnpaulsen10@web.de>
2023-10-20 09:31:49 +0200

    feinschliff

--- src/components/Testkomponente.vue
+++ src/components/Testkomponente.vue
@@ -1,6 +1,6 @@
 <template>
 
-<h1>Hallo {{props.props}}</h1>
+<h1>Hallo, ich habe Hunger. Bitte füttere mich mit neuen ToDos, wenn du diese abschließt bin ich dazu nicht nur satt sondern auch glücklich. {{props.props}}</h1>
 </template> 
 
 <script setup>
