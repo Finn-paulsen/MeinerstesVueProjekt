53d73d1d45f4e789ada75c02881cf203a69a97b7
Finn-paulsen <Finnpaulsen10@web.de>
2023-10-23 19:43:49 +0200

    all

--- src/components/Testkomponente.vue
+++ src/components/Testkomponente.vue
@@ -1,6 +1,6 @@
 <template>
 
-<h1>Hallo, ich habe Hunger. Bitte füttere mich mit neuen ToDos, wenn du diese abschließt bin ich dazu nicht nur satt sondern auch glücklich. {{props.props}}</h1>
+<h1>Wie lautet deine neue To-Do? {{props.props}}</h1>
 </template> 
 
 <script setup>
